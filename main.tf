28183d9d4c49371827a4328e7407b256ed8b5d05
ucipass@gmail.com <ucipass@gmail.com>
2021-08-11 14:11:33 -0500

    test

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.AWS_REGION
 }
 
-module "vpc1" {
-  source = "./modules/vpc"
-  vpc_name = var.VPC_NAME
-  vpc_cidr = var.VPC_CIDR
-}
+# module "vpc1" {
+#   source = "./modules/vpc"
+#   vpc_name = var.VPC_NAME
+#   vpc_cidr = var.VPC_CIDR
+# }
