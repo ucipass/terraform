bb857705deaa6520fb78dc5de958b5ef216ab241
ucipass@gmail.com <ucipass@gmail.com>
2021-08-11 17:52:23 -0500

    sg7

--- main.tf
+++ main.tf
@@ -39,8 +39,6 @@ module "vote_service_sg" {
   ingress_rules            = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
     {
-      from_port   = 8080
-      to_port     = 8090
       protocol    = "tcp"
       description = "User-service ports"
       cidr_blocks = "10.10.0.0/16"
