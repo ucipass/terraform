0166f678511508af775c27e76af8b13c5465d6c0
ucipass@gmail.com <ucipass@gmail.com>
2021-08-12 17:15:10 -0500

    dn2

--- main.tf
+++ main.tf
@@ -8,24 +8,24 @@ provider "aws" {
 #   vpc_cidr = var.VPC_CIDR
 # }
 
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-#   name = var.VPC_NAME
-#   cidr = "10.0.0.0/16"
+  name = var.VPC_NAME
+  cidr = "10.0.0.0/16"
 
-#   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-#   enable_nat_gateway = false
-#   enable_vpn_gateway = false
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
-#   tags = {
-#     Terraform = "true"
-#     Environment = "dev"
-#   }
-# }
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 
 # module "vote_service_sg" {
 #   depends_on = [module.vpc]
