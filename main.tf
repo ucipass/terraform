03a796ddcf9c80710eaa3bd642ea0e041e40bc81
ucipass@gmail.com <ucipass@gmail.com>
2021-08-23 18:58:57 -0500

    ec2-16

--- main.tf
+++ main.tf
@@ -79,7 +79,8 @@ module "ec2_cluster" {
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
-  zone_name = "aws.cooltest.site"
+  count=2
+  zone_name = "aws${count.index}.cooltest.site"
 
   records = [
     {
@@ -87,7 +88,7 @@ module "records" {
       type    = "A"
       ttl     = 3600
       records = [
-        module.ec2_cluster.public_ip[0],
+        module.ec2_cluster.public_ip[count.index],
       ]
     },
   ]
