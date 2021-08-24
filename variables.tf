f3987e9a9e91488683a116edb103acf212db145f
ucipass@gmail.com <ucipass@gmail.com>
2021-08-24 10:02:08 -0500

    1002

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
+variable "NAME" {
+  default="AA"
+  type = string
+}
 variable "AWS_REGION" {
   default="us-east-2"
   type = string
 }
 
-variable "NAME" {
-  default="AA"
-  type = string
-}
 variable "COUNT" {
   default= "2"
   type = string
