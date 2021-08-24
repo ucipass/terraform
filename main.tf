533218a01aae6f70ec1046ed856c02d05e65d603
ucipass@gmail.com <ucipass@gmail.com>
2021-08-23 20:17:04 -0500

    2016

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "ec2_cluster" {
   version                = "~> 2.0"
 
   name                   = var.NAME
-  instance_count         = var.COUNT
+  instance_count         = 1
 
   ami                    = "ami-0279406e0655775be" 
   instance_type          = "t2.micro"
@@ -76,7 +76,7 @@ module "ec2_cluster" {
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
-  count=var.COUNT
+  count=1
   zone_name = "aws.cooltest.site"
 
   records = [
