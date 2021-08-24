e448e32fea23128fa7053c61763dce0b9e87362e
ucipass@gmail.com <ucipass@gmail.com>
2021-08-24 13:59:55 -0500

    1359

--- main.tf
+++ main.tf
@@ -61,22 +61,7 @@ module "custom_sg" {
   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = module.vpc.vpc_id
 
-  egress_cidr_blocks      = ["0.0.0.0/0"]
-  ingress_cidr_blocks      = ["10.0.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = -1
-      to_port     = -1
-      protocol    = -1
-      description = "User-service ports"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
+  ingress_cidr_blocks = ["10.10.0.0/16"]
 }
 
 module "ec2_cluster_ub" {
